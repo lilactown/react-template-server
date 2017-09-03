180c52b7e5e9daffc444deb3ae62aa354acbdf1a
Will <will@willacton.com>
2017-09-03 11:56:34 -0700

    initialize counter app state

--- components/pages/counter/app.js
+++ components/pages/counter/app.js
@@ -6,16 +6,22 @@ import asset from "../../common/asset";
 export default class CounterApp extends Component {
   constructor(props) {
     super(props);
-    // initialize state with initial props passed in
-    this.state = {
-      count: props.count ? props.count : 0,
-      name: props.name ? props.name : ""
-    };
+    if (typeof window !== "undefined" && window.__COUNTER_APP_DATA__) {
+      console.log("state initializing");
+      this.state = window.__COUNTER_APP_DATA__;
+    } else {
+      // initialize state with initial props passed in
+      this.state = {
+        count: props.count ? props.count : 0,
+        name: props.name ? props.name : ""
+      };
+    }
   }
 
   changeName = ev => this.setState({ name: ev.target.value });
 
   incCount = () => this.setState(({ count }) => ({ count: count + 1 }));
+  decCount = () => this.setState(({ count }) => ({ count: count - 1 }));
 
   render() {
     return (
@@ -28,6 +34,7 @@ export default class CounterApp extends Component {
             Counter: {this.state.count}
             <div>
               <button onClick={this.incCount}>+</button>
+              <button onClick={this.decCount}>-</button>
             </div>
           </div>
         </div>
@@ -37,8 +44,19 @@ export default class CounterApp extends Component {
 }
 
 export function Page(props) {
+  const initialState = {
+    count: props.count ? parseInt(props.count, 10) : 0,
+    name: props.name ? props.name : ""
+  };
   return (
     <BasicLayout>
+      {/* Preload our state */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.__COUNTER_APP_DATA__ = ${JSON.stringify(initialState)};`
+        }}
+      />
       <div
         id="app"
         dangerouslySetInnerHTML={{
